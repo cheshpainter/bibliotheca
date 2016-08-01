2bc85728b4fa57b587009bea1f8a38ad52b717f5
paintem <michael.painter@thomsonreuters.com>
2016-08-01 12:15:53 +0100

    Extend api.

--- src/server/routes/authors.js
+++ src/server/routes/authors.js
@@ -3,112 +3,164 @@
 module.exports = (function () {
 
     var express = require('express');
-    var authors = express.Router();
+    var router = express.Router();
 
     var models = require("../models"),
         Book = models.Book,
-        Author = models.Author;
+        Edition = models.Edition,
+        Format = models.Format,
+        Author = models.Author,
+        sequelize = models.sequelize;
 
-    var getAllAuthors = function (req, res, next) {
+    router.route('/').get(findAll).post(createOne);
+
+    router.route('/:authorid').get(findOne).put(updateOne);
+
+    router.route('/:authorid/hasWritten/:bookid').put(updateOneAuthorship);
+
+    function findAll(req, res) {
 
         Author.findAll().then(function (authors) {
             // No results returned mean the object is not found
             if (authors.length === 0) {
                 // We are able to set the HTTP status code on the res object
-                res.statusCode = 404;
-                return res.json({
-                    errors: ["Authors not found"]
+                res.status(404).json({
+                    status: "error",
+                    message: "Authors not found"
                 });
             }
-            req.authors = authors;
-            next();
+
+            var pojos = [];
+
+            authors.forEach(function (author) {
+
+                var pojo = author.get({
+                    plain: true
+                });
+
+                pojo.links = {};
+
+                pojos.push(pojo);
+
+            });
+
+            res.status(200)
+                .json({
+                    data: pojos,
+                    status: 'success',
+                    message: 'Retrieved all authors'
+                });
+
+
         }).catch(function (err) {
             if (err) {
                 console.error(err);
-                res.statusCode = 500;
-                return res.json({
-                    errors: ["Could not retrieve authors"]
+                res.status(500).json({
+                    status: "error",
+                    message: "Could not retrieve authors"
                 });
             }
         });
     }
 
-    var getOneAuthor = function (req, res, next) {
+    function createOne(req, res) {
 
-        var authorId = req.params.id;
+        var author = req.body;
 
-        Author.findById(authorId).then(function (author) {
-            // No results returned mean the object is not found
-            if (author === null) {
-                // We are able to set the HTTP status code on the res object
-                res.statusCode = 404;
-                return res.json({
-                    errors: ["Author not found"]
-                });
-            }
-            req.author = author;
-            next();
-        }).catch(function (err) {
-            if (err) {
-                console.error(err);
-                res.statusCode = 500;
-                return res.json({
-                    errors: ["Could not retrieve author"]
-                });
-            }
-        });
-    }
-
-    var getAllBooksForOneAuthor = function (req, res, next) {
-
-        var authorId = req.params.id;
-
-        Book.findAll({
-            include: [{
-                model: Author,
-                as: 'writtenBy',
-                attributes: ['id'],
-                through: {
-                    attributes: []
-                },
-                where: {
-                    id: authorId
-                }
+        Author.findAll({
+            attributes: ['id'],
+            where: [{
+                name: author.name
             }]
-        }).then(function (books) {
-            // No results returned mean the object is not found
-            if (books.length === 0) {
-                // We are able to set the HTTP status code on the res object
-                res.statusCode = 404;
-                return res.json({
-                    errors: ["Books not found"]
+        }).then(function (authors) {
+
+            if (authors.length === 0) {
+
+                Author.create(author).then(function (author) {
+                    res.status(200)
+                        .json({
+                            data: {
+                                links: {
+                                    authors: ['/authors/' + author.id]
+                                }
+                            },
+                            status: 'success',
+                            message: 'Created one author'
+                        });
+                });
+
+            } else {
+
+                var author = authors[0];
+                return res.status(404).json({
+                    data: {
+                        links: {
+                            books: ['/author/' + author.id]
+                        }
+                    },
+                    status: 'error',
+                    message: "Author not created; resource already exists"
                 });
             }
-            req.books = books;
-            next();
         }).catch(function (err) {
             if (err) {
                 console.error(err);
-                res.statusCode = 500;
-                return res.json({
-                    errors: ["Could not retrieve books"]
+                return res.status(500).json({
+                    status: 'error',
+                    message: 'Could not create author'
                 });
             }
         });
 
     }
 
-    authors.get('/', [getAllAuthors], function (req, res) {
-        res.json(req.authors);
-    });
+    function findOne(req, res) {
 
-    authors.get('/:id', [getOneAuthor], function (req, res) {
-        res.json(req.author);
-    });
+        var authorId = req.params.id;
 
-    authors.get('/:id/books', [getAllBooksForOneAuthor], function (req, res) {
-        res.json(req.books);
-    });
+        Author.findAll({
+            where: [{
+                id: authorId
+            }]
+        }).then(function (authors) {
+            // No results returned mean the object is not found
+            if (authors.length === 0) {
+                // We are able to set the HTTP status code on the res object
+                res.status(404).json({
+                    status: "error",
+                    message: "Author not found"
+                });
+            }
+
+            var author = authors[0];
+
+            var pojo = author.get({
+                plain: true
+            });
+
+            pojo.links = {};
+
+            res.status(200)
+                .json({
+                    data: pojo,
+                    status: 'success',
+                    message: 'Retrieved one author'
+                });
+
+        }).catch(function (err) {
+            if (err) {
+                console.error(err);
+                res.status(500).json({
+                    status: "error",
+                    message: "Could not retrieve author"
+                });
+            }
+        });
+    }
+
+    function updateOne(req, res) {
+
+    }
 
     return authors;
 })();
