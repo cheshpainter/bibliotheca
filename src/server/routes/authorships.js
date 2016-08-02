fa5fbac6d864d1592febe84855b5630d7b2442a8
paintem <michael.painter@thomsonreuters.com>
2016-08-02 15:36:33 +0100

    Updated.

--- src/server/routes/authorships.js
+++ src/server/routes/authorships.js
@@ -20,8 +20,29 @@ module.exports = (function () {
     }
 
     function updateOneAuthorship(req, res) {
+
         var bookId = req.query.bookid;
         var authorId = req.query.authorid;
+
+        Promise.all([Book.findAll({
+            where: [{
+                id: bookId
+            }]
+        }), Author.findAll({
+            where: [{
+                id: authorId
+            }]
+        })]).spread(function (books, authors) {
+
+            books[0].addWrittenBy(authors[0]);
+            books[0].save().then(function () {
+                res.status(200).json({
+                    data: {},
+                    status: "success",
+                    message: "Update nothing"
+                });
+            });
+        });
     }
 
     return router;
