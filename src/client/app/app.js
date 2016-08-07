0df3ef7a99035c73ea79ef2ca348d717e0293e81
Michael Painter <michael@Chesh-Painters-MacBook.local>
2016-08-07 11:22:26 +0100

    Replace ngroute with ui-router.

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -2,15 +2,27 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
-
-    $routeProvider.otherwise({
-        redirectTo: '/view1'
-    });
-}]);
+]).config(function ($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/view1");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('view1', {
+            url: "/view1",
+            templateUrl: "view1/view1.html",
+            controller: 'View1Ctrl',
+            controllerAs: 'view1Ctrl'
+        })
+        .state('view2', {
+            url: "/view2",
+            templateUrl: "view2/view2.html",
+            controller: 'View2Ctrl',
+            controllerAs: 'view2Ctrl'
+        });
+});
