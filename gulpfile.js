66845ad1858dc1a8b43534ffd714f50a2d445eee
Michael Painter <michael@Chesh-Painters-MacBook.local>
2016-08-10 15:00:23 +0100

    Basic do nothing gulp setup.

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var plug = require('gulp-load-plugins')();
+
+/**
+ * List the available gulp tasks
+ */
+gulp.task('help', plug.taskListing);
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
